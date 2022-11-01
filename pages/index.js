cada0f7e130e950b3c8aec2eaa989c08c6556fb1
bstewart3 <505propz@gmail.com>
2022-11-01 10:25:47 -0700

    add routes add navbar

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
+
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
+import Loader from '../components/Loader'
 
 export default function Home() {
   return (
     <div >
-
+      <Loader show />
     </div>
   )
 }
