c31823be89b6b93be1d62db9883b98281d4c2e1d
bstewart3 <505propz@gmail.com>
2022-11-01 12:53:54 -0700

    add context

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,17 @@
 import '../styles/globals.css'
 import Navbar from '../components/Nav';
 import { Toaster } from 'react-hot-toast';
+import { UserContext } from '../lib/context';
 
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-        <Navbar />
-        <Component {...pageProps} />
-        <Toaster />
+      <UserContext.Provider value={{ user: {}, username: 'brahh-nin'}}>
+          <Navbar />
+          <Component {...pageProps} />
+          <Toaster />
+      </UserContext.Provider>
     </>
   ) 
 }
