491684d9d6f65eb3c85a12deaae41e8d8e508cbb
bstewart3 <505propz@gmail.com>
2022-11-02 16:41:18 -0700

    add useEffect

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,33 @@ import Navbar from "../components/Nav";
 import { Toaster } from "react-hot-toast";
 import { UserContext } from "../lib/context";
 
+import {useState, useEffect} from 'react';
+import {useAuthState} from 'react-firebase-hooks/auth'
+
 function MyApp({ Component, pageProps }) {
+
+  const [user] = useAuthState(auth);
+  const [username, setUserName] = useState(null)
+
+  useEffect(() => {
+    // turn off realtime  subscription
+    let unsubscribe;
+
+    if(user) {
+      const ref = firestore.collection('users').doc(user.uid);
+      unsubscribe = ref.onSnapshot((doc) => {
+        setUserName(doc.data()?.username)
+      })
+    } else {
+      setUserName(null)
+    }
+
+  },[user])
+  
+
   return (
     <>
-      <UserContext.Provider value={{ user: {}, username: "brahh-nin" }}>
+      <UserContext.Provider value={{ user, username }}>
         <Navbar />
         <Component {...pageProps} />
         <Toaster />
