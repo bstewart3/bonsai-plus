a3f6b76c97e57ef20deefaadccd948d91c453aad
bstewart3 <505propz@gmail.com>
2022-11-21 11:15:38 -0800

    add custom 404 and real time hydration

--- pages/[username]/[slug].js
+++ pages/[username]/[slug].js
@@ -1,4 +1,9 @@
+import PostContent from "../../components/PostContent";
+import styles from "../../styles/Post.module.css";
+
 import { firestore, getUserWithUserName, postToJSON } from "../../lib/firebase";
+
+import { useDocumentData } from "react-firebase-hooks/firestore";
 
 export async function getStaticProps({ params }) {
   const { username, slug } = params;
@@ -41,5 +46,22 @@ export async function getStaticPaths() {
 }
 
 export default function Post(props) {
-  return <main></main>;
+  const postRef = firestore.doc(props.path);
+  const [realtimePost] = useDocumentData(postRef);
+
+  const post = realtimePost || props.post;
+
+  return (
+    <main className={styles.container}>
+      <section>
+        <PostContent post={post} />
+      </section>
+
+      <aside className="card">
+        <p>
+          <strong>{post.heartCount || 0} 🖤</strong>
+        </p>
+      </aside>
+    </main>
+  );
 }
