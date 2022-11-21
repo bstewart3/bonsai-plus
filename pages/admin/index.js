ec04ca302457e75d505898caad702affbb394207
bstewart3 <505propz@gmail.com>
2022-11-21 12:41:46 -0800

    add MetaTag component for seo

---
+++ pages/admin/index.js
@@ -0,0 +1,10 @@
+import MetaTags from "../../components/Metatags";
+
+export default function AdminPostsPage({}) {
+  return (
+    <main>
+      <MetaTags title="admin page" />
+      <h1>Edits Posts</h1>
+    </main>
+  );
+}
