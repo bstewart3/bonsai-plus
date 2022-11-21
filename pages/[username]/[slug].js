d1884df6ce9d23d5db615b9520f8ef9b0ca54fe1
bstewart3 <505propz@gmail.com>
2022-11-21 10:42:43 -0800

    add ISR to [slug].js

--- pages/[username]/[slug].js
+++ pages/[username]/[slug].js
@@ -1,7 +1,45 @@
-export default function AminPostPage({ }) {
-    return (
-        <main>
+import { firestore, getUserWithUserName, postToJSON } from "../../lib/firebase";
 
-        </main>
-    )
+export async function getStaticProps({ params }) {
+  const { username, slug } = params;
+  const userDoc = await getUserWithUserName(username);
+
+  let post;
+  let path;
+
+  if (userDoc) {
+    const postRef = userDoc.ref.collection("posts").doc(slug);
+    post = postToJSON(await postRef.get());
+
+    path = postRef.path;
+
+    return {
+      props: { post, path },
+      revalidate: 5000,
+    };
+  }
+}
+
+export async function getStaticPaths() {
+  const snapshot = await firestore.collectionGroup("posts").get();
+
+  const paths = snapshot.docs.map((doc) => {
+    const { slug, username } = doc.data;
+    return {
+      params: { username, slug },
+    };
+  });
+  return {
+    // must be in this format:
+    //paths: [
+    //  { params: { username, slug}}
+    // ],
+
+    paths,
+    fallback: "blocking",
+  };
+}
+
+export default function Post(props) {
+  return <main></main>;
 }
